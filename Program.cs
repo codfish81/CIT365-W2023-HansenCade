c18dcd1ac1732816292856cc556d57c5231231ca
codfish81 <codfish81@byui.edu>
2023-01-11 10:40:21 -0700

    Deleted extra space

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         string myName, myLocation;
-        Console.WriteLine("Enter  your first name: ");
+        Console.WriteLine("Enter your first name: ");
         myName = Console.ReadLine();
         Console.WriteLine("Enter your location: ");
         myLocation = Console.ReadLine();
